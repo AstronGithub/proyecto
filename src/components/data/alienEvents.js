1a23ca3fb121971da8696f801fb28fb8963f8ba2
AndresMangano <andresms3992@hotmail.com>
2023-09-16 14:26:42 -0300

    Mock data

---
+++ src/components/data/alienEvents.js
@@ -0,0 +1,42 @@
+export const alienEvents = [
+    {
+        Image: "https://i.guim.co.uk/img/media/7524fd1c33d10dba057a272429d25b4343e3170f/0_23_3500_2101/master/3500.jpg?width=620&quality=85&auto=format&fit=max&s=2d66b70a8c89eb1cba9cb4062c6ee998",
+        Name: "Storm Area 52: The Sequel",
+        Description: "Following the viral success of the 'Storm Area 51' event, a new social media movement proposes a gathering at the lesser-known Area 52. The intention is to demand transparency from the government on any potential extraterrestrial findings.",
+        Location: "Area 52, Nevada, USA",
+        ExpectedAttendance: "Estimated 5,000 attendees (as the hype is building upon the previous event)",
+        Objective: "A peaceful assembly to further raise awareness and seek the truth about extraterrestrial life and the government's involvement."
+    },
+    {
+        Image: "https://link-to-image-of-GlobalUFOConference2025.com",
+        Name: "https://s.abcnews.com/images/Technology/GTY_ellen_stofan_kab_150408_16x9_1600.jpg",
+        Description: "A worldwide conference inviting scientists, researchers, and enthusiasts from around the world to discuss the most recent UFO sightings, theories, and potential evidence of extraterrestrial life.",
+        Location: "Paris, France at the International Space Center",
+        ExpectedAttendance: "Over 10,000 attendees from all continents, including renowned scientists and researchers.",
+        Objective: "To share knowledge, evidence, and theories about UFOs and extraterrestrial life in an academic setting, fostering global collaboration."
+    },
+    {
+        Image: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2017_18/1989441/proximab.jpg",
+        Name: "MarsCon 2026: Extraterrestrial Life Possibilities",
+        Description: "A convention focusing on the latest findings from Mars, particularly regarding the potential signs of past or present life. Scientists will present their latest research and findings.",
+        Location: "Los Angeles, California at the Space Research Institute",
+        ExpectedAttendance: "Around 8,000 attendees, primarily from the scientific community.",
+        Objective: "Evaluate and share the current status of Mars research, especially focusing on the search for life."
+    },
+    {
+        Image: "https://pbs.twimg.com/media/EwqBwO8UUAEjb3p.png",
+        Name: "Deep Space Communications Symposium",
+        Description: "A symposium that dives into the world of sending and receiving signals from deep space. One of the main sessions is about potential extraterrestrial signals we might have received.",
+        Location: "Berlin, Germany at the Deep Space Communications Center",
+        ExpectedAttendance: "Expected 5,500 attendees ranging from communication experts to curious enthusiasts.",
+        Objective: "Discuss methods, technologies, and any potential extraterrestrial communications detected."
+    },
+    {
+        Image: "https://filmfreeway-production-storage-01-storage.filmfreeway.com/festivals/logos/000/070/733/large/logo.jpg?1672330794",
+        Name: "UFO Film Festival 2027",
+        Description: "A film festival dedicated to documentaries and movies centered around UFOs and extraterrestrial encounters. The event will feature prominent directors and possibly real-life witnesses.",
+        Location: "Toronto, Canada at the Modern Arts Center",
+        ExpectedAttendance: "Over 6,000 attendees including many from the film industry.",
+        Objective: "Celebrate cinematic achievements that shed light on the UFO phenomena and encourage more content creators to explore this theme."
+    }
+];
---
+++ src/components/data/alienNews.js
@@ -0,0 +1,123 @@
+export const alienNews = [
+    {
+      image: "https://images.ladbible.com/resize?type=jpeg&quality=70&width=1200&fit=contain&gravity=auto&url=https://images.ladbiblegroup.com/v3/assets/blt949ea8e16e463049/bltfdd999eab07cb6b1/650180e7ceedbe512bb30b2f/alien-bodies-1000-years-old-mexico.png",
+      title: "Mexico legislators hold hearing on existence of UFOs, shown ‘alien’ bodies",
+      content: `
+      Journalist and self-proclaimed UFO scientist Jaime Maussan recently presented fossilized bodies he claims to be of "alien" origin in a special session of the Mexican Congress. The bodies were reportedly recovered from Cusco, Peru, and are said to be around 1,000 years old. Maussan argued that they are not from Earth and were found fossilized in a diatom moss mine; he further stated that they were analyzed by the National Autonomous University of Mexico (UNAM), which managed to extract DNA samples and conduct radiocarbon testing.
+
+      Despite Maussan's sensational claims, which were backed by former U.S. pilot Ryan Graves and Harvard astrophysicist Avi Loeb, the scientific community remains skeptical. The fossils were found to be covered with a layer of sand, and Maussan's allegations have yet to be scientifically validated. Moreover, Maussan has been associated with previously debunked discoveries, casting further doubt on his latest revelation.
+      
+      The session in Mexico followed a similar event in front of the U.S. Congress, signaling a growing interest in unidentified phenomena within both scientific and political circles. However, there remains significant disagreement on the origin of such phenomena, and while it's generally accepted that there are unidentified phenomena in the universe, their exact nature is still hotly debated.
+      
+      Given Maussan's past involvement in a debunked high-profile hoax concerning the discovery of fossilized remains in Nazca, Peru, his current claims are being taken with a grain of salt. The previously "discovered" body with an elongated skull, which Maussan claimed to be a mummified alien, was later revealed to be a human child whose head had been bound in an ancient practice, according to fact-checking website Snopes.com.
+      `,
+      author: "Daniel Martinez",
+      country: "USA"
+    },
+    {
+      image: "https://cdn.images.express.co.uk/img/dynamic/151/590x/NASA-alien-life-news-habitable-planet-exoplanet-discovery-957795.jpg?r=1686998680160",
+      title: "NASA releases UFO report, appoints unidentified object research director",
+      content: `
+      NASA has introduced a new role to oversee the study of unidentified anomalous phenomena (UAP), commonly referred to as UFOs. In a recent report, the space agency disclosed findings from a team assembled to strategize on how NASA can investigate future UAP sightings. Shortly after the announcement, Mark McInerney was unveiled as the new director of UAP research.
+
+      In June 2022, NASA formed a 16-member independent team to scrutinize UAP incidents that couldn't be identified as aircraft or natural occurrences. Their objective wasn't to pinpoint the origins of these mysterious objects but to chart a path for NASA's future exploration and study of such phenomena.
+      
+      NASA Administrator Bill Nelson clarified that the research group found no evidence suggesting that UAPs have extraterrestrial origins. However, the nature of these phenomena remains unknown. The team's efforts concentrated on devising methods to examine UAPs by leveraging existing data, technology, and scientific tools.
+      
+      NASA characterizes UAPs as unidentifiable events observed in the sky, distinct from known aircraft or natural phenomena. The agency notes that the sparse high-quality observations of these occurrences hinder firm scientific conclusions regarding their essence.
+      `,
+      author: "Isabella Ferreira",
+      country: "Brazil"
+    },
+    {
+        image: "https://image.ondacero.es/clipping/cmsimages01/2015/02/02/CB413082-4559-4D28-8AE7-A8070055B6DD/69.jpg?crop=682,384,x0,y8&width=1280&height=720&optimize=low&format=webply",
+        title: "Mysterious Crop Circles Appear Overnight in UK",
+        content: `
+          Farmers in Wiltshire, UK, woke up to a perplexing sight as intricate crop circles appeared overnight in several wheat fields. These designs, some spanning hundreds of meters, show complex geometric patterns that some believe are too intricate to be made by humans without being noticed.
+          
+          Crop circles in the UK have been a topic of intrigue for decades, with many attributing them to extraterrestrial activity. Critics argue they're man-made, often as pranks or art installations.
+          
+          "I've had my field for over 20 years, and I've never seen anything like this," commented farmer Giles Harrington. The local police and government officials are investigating the phenomenon.
+          
+          As of now, no one has claimed responsibility for the crop circles, leaving residents and experts alike puzzled about their mysterious origin.
+        `,
+        author: "Lucy Redwood",
+        country: "UK"
+      },
+      {
+        image: "https://thedaily.case.edu/wp-content/uploads/2023/07/astronaut-standing-on-the-moon.jpg",
+        title: "Astronauts Report Strange Lights on Moon's Dark Side",
+        content: `
+          During a routine mission around the moon, astronauts from the European Space Agency reported seeing strange, fleeting lights on the moon's dark side. These lights, which were captured on video, appeared to move in a coordinated pattern.
+          
+          While it's not the first time astronauts have reported unexplained phenomena during space missions, it's rare for such incidents to be captured on camera. The footage has reignited public interest in what lies on the lesser-known side of the moon.
+          
+          "It was unlike anything I've ever seen," astronaut Claire Dubois mentioned during a post-mission interview. The ESA, in collaboration with NASA, is examining the footage and conducting further investigations.
+          
+          Speculations range from reflections of space debris to undiscovered moon phenomena, and yes, extraterrestrial activity. The mystery remains unsolved.
+        `,
+        author: "Henrik Olsen",
+        country: "Germany"
+      },
+      {
+        image: "https://i.ndtvimg.com/i/2015-06/mars-pyramid_650x400_61434975068.jpg",
+        title: "Pyramid-like Structures Discovered on Mars?",
+        content: `
+          Recent satellite images of Mars have revealed structures that eerily resemble pyramids on Earth. Located in a previously unexplored Martian canyon, the pyramid-like formations appear to be aligned similarly to the pyramids of Giza in Egypt.
+          
+          Scientists are both excited and baffled. While some believe this could be a groundbreaking discovery indicating past life or even civilizations on Mars, others argue they might just be natural rock formations.
+          
+          "Mars continues to surprise us. Whether these are signs of ancient life or simply unique geological formations, they warrant further study," says Dr. Rajan Pillai, a planetary geologist.
+          
+          Space agencies worldwide are buzzing with excitement, and there are already talks about potential missions to investigate these structures. The world waits with bated breath.
+        `,
+        author: "Aisha Patel",
+        country: "India"
+      },
+      {
+        image: "https://i.dailymail.co.uk/i/pix/2009/06/03/article-1190481-0531795D000005DC-401_634x419.jpg",
+        title: "Unusual Lights in Australian Outback Spark UFO Theories",
+        content: `
+          Remote towns in the Australian Outback have reported unusual light phenomena in the night sky. These aren't your regular shooting stars, but orchestrated lights dancing in patterns, then disappearing into the horizon.
+          
+          While some local residents believe this is linked to extraterrestrial visitors, others think it might be related to natural atmospheric conditions unique to the region.
+          
+          "It felt like a show just for us. The lights danced and shimmered for almost an hour," described Abigail Holt, a resident of Alice Springs. Local authorities are consulting with space agencies to get a possible explanation.
+          
+          This event has led to a surge in tourists, hoping to catch a glimpse of these mysterious lights, turning the usually quiet Outback towns into hubs of excitement and speculation.
+        `,
+        author: "Liam Turner",
+        country: "Australia"
+      },
+      {
+        image: "https://previews.agefotostock.com/previewimage/medibigoff/1369ba6398541563bd04a15ff35275ba/zm4-2008919.jpg",
+        title: "Ancient Alien Artifacts or Earthly Relics? Debate Sparks in Egypt",
+        content: `
+          Archaeologists in Egypt stumbled upon artifacts that don't quite fit the narrative of ancient Egyptian civilization. These metallic objects, shaped like discs and emitting a faint glow, have left experts puzzled.
+          
+          Enthusiasts of ancient alien theories suggest these could be remnants of extraterrestrial visitors from the past. Mainstream archaeologists, however, caution against such interpretations without concrete evidence.
+          
+          "It's certainly an unprecedented discovery. But to jump to extraterrestrial conclusions is premature," states Dr. Yasmine Farouk, a lead archaeologist on the site.
+          
+          As experts from around the world flock to Egypt to study these artifacts, the world watches keenly, awaiting more answers about these mysterious objects.
+        `,
+        author: "Mohamed Safwat",
+        country: "Egypt"
+      },
+      {
+        image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/2D3F/production/_111138511_codename_rendlesham1_chillfactorfilms.jpg",
+        title: "Canadian Forests: A New UFO Hotspot?",
+        content: `
+          Dense forests in Northern Canada have become the latest UFO hotspot, with numerous sightings reported in the last six months. These reports include descriptions of saucer-shaped objects, bright lights, and even accounts of potential encounters.
+          
+          The Canadian government has launched an official investigation, collaborating with indigenous communities who've resided in these regions for centuries and might have historical accounts or explanations.
+          
+          "Our ancestors have tales of star people. Maybe they're not just tales," mentions Nyla Cardinal, an indigenous community leader.
+          
+          With a mix of modern sightings and ancient indigenous stories, Northern Canada's forests are now the focus of ufologists and curious travelers, all hoping to unravel the mysteries lurking among the trees.
+        `,
+        author: "Sophie Labelle",
+        country: "Canada"
+      }
+  ];
+    
