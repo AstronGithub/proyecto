774fcf3f07816ee23e3e155dc6d11a9db7362f7b
AndresMangano <andresms3992@hotmail.com>
2023-09-17 18:34:22 -0300

    Primeras letras en minuscula

--- src/data/alienRaces.js
+++ src/data/alienRaces.js
@@ -1,75 +1,75 @@
 export const alienRaces = [
     {
-      Image: "https://image.api.playstation.com/vulcan/ap/rnd/202302/0309/db44f801bcfe5b7b5ba8e1369897910819b8168dab31652b.png",
-      Name: "Greys",
-      Description: "Short, humanoid beings with large heads and oversized, almond-shaped, black eyes. Their skin is a greyish color. They are often the most depicted aliens in media, known for abductions.",
-      Origin: "Zeta Reticuli star system",
-      Population: "Unknown",
-      Hostility: 5,
-      Objective: "Studying human species and possibly hybridizing with humans for survival reasons."
+      image: "https://image.api.playstation.com/vulcan/ap/rnd/202302/0309/db44f801bcfe5b7b5ba8e1369897910819b8168dab31652b.png",
+      name: "Greys",
+      description: "Short, humanoid beings with large heads and oversized, almond-shaped, black eyes. Their skin is a greyish color. They are often the most depicted aliens in media, known for abductions.",
+      origin: "Zeta Reticuli star system",
+      population: "Unknown",
+      hostility: 5,
+      objective: "Studying human species and possibly hybridizing with humans for survival reasons."
     },
     {
-      Image: "https://pbs.twimg.com/media/Ebdq_U8WkAEzFDb.jpg",
-      Name: "Reptilians",
-      Description: "Tall, reptile-like beings. They are believed to have a humanoid appearance, but with scaly, reptilian skin. Conspiracy theories often associate them with world domination schemes.",
-      Origin: "Alpha Draconis star system",
-      Population: "Unknown",
-      Hostility: 8,
-      Objective: "Expansion of their empire and purported manipulation of human societies."
+      image: "https://pbs.twimg.com/media/Ebdq_U8WkAEzFDb.jpg",
+      name: "Reptilians",
+      description: "Tall, reptile-like beings. They are believed to have a humanoid appearance, but with scaly, reptilian skin. Conspiracy theories often associate them with world domination schemes.",
+      origin: "Alpha Draconis star system",
+      population: "Unknown",
+      hostility: 8,
+      objective: "Expansion of their empire and purported manipulation of human societies."
     },
     {
-        Image: "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/6235154e48b99f001d83a58e.jpg",
-        Name: "Pleiadians",
-        Description: "Humanoid beings that resemble humans in appearance, but are of higher spiritual development. Often considered benevolent and interested in the spiritual growth of humanity.",
-        Origin: "Pleiades star cluster",
-        Population: "Unknown",
-        Hostility: 2,
-        Objective: "Guide and assist humanity in its spiritual evolution."
+      image: "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/6235154e48b99f001d83a58e.jpg",
+      name: "Pleiadians",
+      description: "Humanoid beings that resemble humans in appearance, but are of higher spiritual development. Often considered benevolent and interested in the spiritual growth of humanity.",
+      origin: "Pleiades star cluster",
+      population: "Unknown",
+      hostility: 2,
+      objective: "Guide and assist humanity in its spiritual evolution."
       },
-      {
-        Image: "https://i.etsystatic.com/20731841/r/il/4a821c/4221648021/il_fullxfull.4221648021_l3zc.jpg",
-        Name: "Arcturians",
-        Description: "Advanced alien species known for their high intelligence, spirituality, and abilities in healing. They have blue skin and are shorter than humans.",
-        Origin: "Arcturus star system",
-        Population: "Unknown",
-        Hostility: 1,
-        Objective: "Spread peace and wisdom, and aid other species in their spiritual journeys."
-      },
-      {
-        Image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4ff9dd0d-e3e1-4254-bad7-f267ef833659/dfovwbt-63c032c6-1d3a-426f-b9d8-219fec4245e5.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRmZjlkZDBkLWUzZTEtNDI1NC1iYWQ3LWYyNjdlZjgzMzY1OVwvZGZvdndidC02M2MwMzJjNi0xZDNhLTQyNmYtYjlkOC0yMTlmZWM0MjQ1ZTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.OyX0W1cPlq7jolOjd7ztxyxmgO35WA04cMpmF4C94ro",
-        Name: "Nordics",
-        Description: "Tall, humanoid beings with blonde hair and blue eyes. They resemble Nordic or Scandinavian humans and are often associated with benevolent interactions with humans.",
-        Origin: "Lyra constellation",
-        Population: "Unknown",
-        Hostility: 3,
-        Objective: "Observe and peacefully interact with humanity, sometimes offering guidance."
-      },
-      {
-        Image: "https://as2.ftcdn.net/v2/jpg/05/58/13/39/1000_F_558133902_IDEJeakJKMCqysKd5RtfSWWl7qqQqFcG.jpg",
-        Name: "Anunnaki",
-        Description: "Tall, god-like humanoid beings that are said to have played a role in Sumerian mythology and ancient civilizations. They possess advanced technology and are often considered as creators or teachers.",
-        Origin: "Planet Nibiru",
-        Population: "Unknown",
-        Hostility: 4,
-        Objective: "To mine resources, especially gold, and influence the development of human civilizations."
+    {
+      image: "https://i.etsystatic.com/20731841/r/il/4a821c/4221648021/il_fullxfull.4221648021_l3zc.jpg",
+      name: "Arcturians",
+      description: "Advanced alien species known for their high intelligence, spirituality, and abilities in healing. They have blue skin and are shorter than humans.",
+      origin: "Arcturus star system",
+      population: "Unknown",
+      hostility: 1,
+      objective: "Spread peace and wisdom, and aid other species in their spiritual journeys."
     },
     {
-        Image: "https://i.ytimg.com/vi/iZyO9ulx6Y8/maxresdefault.jpg",
-        Name: "Mantis Beings",
-        Description: "Insectoid creatures resembling the praying mantis. They are often reported by abductees as being present on ships, and they seem to have a high position in the extraterrestrial hierarchy.",
-        Origin: "Somewhere in the Orion constellation",
-        Population: "Unknown",
-        Hostility: 5,
-        Objective: "Scientific research and observation of human behavior and emotions."
+      image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4ff9dd0d-e3e1-4254-bad7-f267ef833659/dfovwbt-63c032c6-1d3a-426f-b9d8-219fec4245e5.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRmZjlkZDBkLWUzZTEtNDI1NC1iYWQ3LWYyNjdlZjgzMzY1OVwvZGZvdndidC02M2MwMzJjNi0xZDNhLTQyNmYtYjlkOC0yMTlmZWM0MjQ1ZTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.OyX0W1cPlq7jolOjd7ztxyxmgO35WA04cMpmF4C94ro",
+      name: "Nordics",
+      description: "Tall, humanoid beings with blonde hair and blue eyes. They resemble Nordic or Scandinavian humans and are often associated with benevolent interactions with humans.",
+      origin: "Lyra constellation",
+      population: "Unknown",
+      hostility: 3,
+      objective: "Observe and peacefully interact with humanity, sometimes offering guidance."
     },
     {
-        Image: "https://i.pinimg.com/originals/e1/92/86/e19286e165f77793d27cbb96ce2e5d7d.jpg",
-        Name: "Aquatic Aliens",
-        Description: "Water-based life forms that are said to live in oceans both on their home planets and possibly Earth. They are humanoid with features resembling aquatic animals like dolphins or amphibians.",
-        Origin: "Hydro-Planet in the Sirius star system",
-        Population: "Unknown",
-        Hostility: 3,
-        Objective: "Exploration of aquatic ecosystems and potentially aiding in the evolution of aquatic species."
+      image: "https://as2.ftcdn.net/v2/jpg/05/58/13/39/1000_F_558133902_IDEJeakJKMCqysKd5RtfSWWl7qqQqFcG.jpg",
+      name: "Anunnaki",
+      description: "Tall, god-like humanoid beings that are said to have played a role in Sumerian mythology and ancient civilizations. They possess advanced technology and are often considered as creators or teachers.",
+      origin: "Planet Nibiru",
+      population: "Unknown",
+      hostility: 4,
+      objective: "To mine resources, especially gold, and influence the development of human civilizations."
+    },
+    {
+      image: "https://i.ytimg.com/vi/iZyO9ulx6Y8/maxresdefault.jpg",
+      name: "Mantis Beings",
+      description: "Insectoid creatures resembling the praying mantis. They are often reported by abductees as being present on ships, and they seem to have a high position in the extraterrestrial hierarchy.",
+      origin: "Somewhere in the Orion constellation",
+      population: "Unknown",
+      hostility: 5,
+      objective: "Scientific research and observation of human behavior and emotions."
+    },
+    {
+      image: "https://i.pinimg.com/originals/e1/92/86/e19286e165f77793d27cbb96ce2e5d7d.jpg",
+      name: "Aquatic Aliens",
+      description: "Water-based life forms that are said to live in oceans both on their home planets and possibly Earth. They are humanoid with features resembling aquatic animals like dolphins or amphibians.",
+      origin: "Hydro-Planet in the Sirius star system",
+      population: "Unknown",
+      hostility: 3,
+      objective: "Exploration of aquatic ecosystems and potentially aiding in the evolution of aquatic species."
     }
   ];
   
