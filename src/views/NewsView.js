a856d4dd8e376958899796816712a45239bda5b8
lucaspalio <lucasivanpallotto@gmail.com>
2023-09-18 11:50:28 -0300

    NewsView.js changes

--- src/views/NewsView.js
+++ src/views/NewsView.js
@@ -1,13 +1,29 @@
 // src/components/NewsView.js
 import React from 'react';
-import { alienRaces } from '../data/alienRaces';
+import { alienNews } from '../data/alienNews';
 import AlienCard from '../components/card/AlienCard';
+import "./NewsView.css"
 
 const NewsView = () => (
   <div className="news-view">
-    {alienRaces.map((alien, index) => (
-      <AlienCard key={index} alien={alien} />
-    ))}
+    {alienNews.map((alien, index) => {
+      // Elimina los espacios adicionales y los saltos de línea, luego divide el contenido en palabras
+      let words = alien.content.replace(/\s+/g, ' ').trim().split(' ');
+
+      // Toma las primeras 20 palabras
+      const shortenedContent = words.slice(0, 20).join(' ');
+
+      return (
+        <AlienCard 
+          key={index} 
+          image={alien.image} 
+          title={alien.title} 
+          content={shortenedContent} // Usa el contenido acortado
+          author={alien.author} 
+          country={alien.country} 
+        />
+      );
+    })}
   </div>
 );
 
