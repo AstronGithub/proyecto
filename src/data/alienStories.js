49ad2206354997e738468d35e1cb48fd34260c92
AndresMangano <andresms3992@hotmail.com>
2023-10-29 15:22:43 -0300

    Backend and stories

---
+++ src/data/alienStories.js
@@ -0,0 +1,59 @@
+export const alienStories = [
+  {
+    id: 1,
+    image: "https://hips.hearstapps.com/hmg-prod/images/area-51-bob-lazar-1563525987.jpg",
+    video: "https://www.youtube.com/watch?v=4UjqFaQq_7I&ab_channel=8NewsNow%E2%80%94LasVegas",
+    shortDescription: "Bob Lazar is known for his claims about reverse-engineering alien UFOs at Area 51. He alleged that he worked in a section called S-4, where UFOs with advanced propulsion systems were being reverse-engineered.",
+    story: `Bob Lazar came into the public eye in the late 1980s when he appeared in television interviews claiming to have been a part of an operation that worked on extraterrestrial technology. He alleged that he was stationed at a site known as S-4, near the infamous Area 51 in Nevada.
+            At S-4, Lazar claimed to have reverse-engineered flying saucers and read government documents describing human-alien interactions. His detailed descriptions about the propulsion of these UFOs, involving gravity waves and an element named "Ununpentium", stirred up much controversy.
+            While many are skeptical of Lazar's claims due to inconsistencies in his stories and lack of evidence, he has stuck to his account over the years, maintaining that he's telling the truth about his experiences at Area 51.`,
+    location: "Area 51, Nevada, USA",
+    witness: {
+      name: "Bob Lazar",
+      nationality: "American"
+    }
+  },
+  {
+    id: 2,
+    image: "https://cropper.watch.aetnd.com/public-content-aetn.video.aetnd.com/video-thumbnails/AETN-History_VMS/21/164/History_UFO_Unexplained_Betty_and_Barney_Hill_SF_HD_1104x622-16x9.jpg",
+    video: "https://www.youtube.com/watch?v=qBcXbYc0zPI&ab_channel=discoveryplus",
+    shortDescription: "Barney and Betty Hill reported being abducted by extraterrestrials in a rural portion of the state of New Hampshire. Their case was the first widely publicized report of an alien abduction in the United States.",
+    story: `The story of Barney and Betty Hill begins on a night in September 1961, when the couple was driving back to their home in Portsmouth, New Hampshire. As they drove, they noticed a bright light in the sky that seemed to be following them. Curious, they stopped the car to get a better look with binoculars.
+            The next thing they remembered was being 35 miles down the road with a lapse of two hours they couldn't account for. Under hypnosis, both the Hills recounted eerily similar stories of being taken aboard an alien craft and examined.
+            Their account, detailed in the book "The Interrupted Journey", was the first widely reported case of alien abduction, making international headlines and introducing many themes that have since become familiar in other alleged encounters.`,
+    location: "White Mountains, New Hampshire, USA",
+    witness: {
+      name: "Barney and Betty Hill",
+      nationality: "American"
+    }
+  },
+  {
+    id: 3,
+    image: "https://play-lh.googleusercontent.com/proxy/w9YTki-sefHQIeSK1TZ8rKDgD7Y9fSoqvZG9oBYtTb0FiAHtEkApp41YlXv_GPu7-KLEFGy5x9NO3_iHdHGNnHZBU7aBptfUe1uz5kaIoYkTVD08cJwS1MZ1Bm5Xo62ykKjdYtn5bD_DtCFfXWJFxijwn2-xrlqV_thyLQ=s1920-w1920-h1080",
+    video: "https://www.youtube.com/watch?v=DsiKEBAFmm4&ab_channel=PowerfulJRE",
+    shortDescription: "Travis Walton claims to have been abducted by a UFO on November 5, 1975, while working with a logging crew in the Apache-Sitgreaves National Forest in Arizona. He reappeared after a five-day search.",
+    story: `On November 5, 1975, Travis Walton was working with a logging crew in the Apache-Sitgreaves National Forest in Arizona. The crew reported seeing a saucer-shaped object hovering over the ground, and Walton approached it out of curiosity. Suddenly, a beam of light struck him and he vanished in front of his colleagues.
+            Walton reappeared five days later, disoriented and confused. Under hypnosis, he recalled being taken aboard the alien craft, where he encountered humanoid beings. He described detailed physical examinations and interactions with various extraterrestrial entities aboard the ship.
+            The Walton case gained significant media attention, with many skeptics pointing to inconsistencies in testimonies. However, Walton and his coworkers have consistently maintained their account of events, making it one of the most well-known and debated abduction cases in UFO history.`,
+    location: "Apache-Sitgreaves National Forest, Arizona, USA",
+    witness: {
+      name: "Travis Walton",
+      nationality: "American"
+    }
+  },
+  {
+    id: 4,
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/20180706_Faith_7_Johnson_Space_Center_1.jpg/905px-20180706_Faith_7_Johnson_Space_Center_1.jpg?20210629083452",
+    video: "https://www.youtube.com/watch?v=KU6gZr9JzlE&ab_channel=SpaceOralHistory",
+    shortDescription: "Gordon Cooper, one of the original seven Mercury astronauts, reported seeing a 'typical saucer shape, double-cylindrical shape, metallic' UFO while flying over Germany in 1951.",
+    story: `Leroy Gordon "Gordo" Cooper Jr. was an American aerospace engineer, test pilot, and one of the seven original astronauts in NASA's Project Mercury. Before his space endeavors, during his time as a fighter pilot at Edwards Air Force Base, Cooper reported witnessing a formation of unidentified objects flying overhead.
+            Later, in 1957, while supervising flight testing at the base, Cooper reported a landed craft that was filmed by his team. The film was handed over to government officials and, according to Cooper, was never returned or discussed.
+            In the years following, Cooper became an advocate for government transparency on the UFO issue. He believed that these unidentified objects were visiting Earth from other planets and advocated for a global effort to research and communicate with these entities. His standing as a respected astronaut lent weight to his testimonies, stirring discussions within both the public and scientific communities.`,
+    location: "Germany",
+    witness: {
+      name: "Gordon Cooper",
+      nationality: "American"
+    }
+  }
+];
+
